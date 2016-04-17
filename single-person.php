51942f16e4bd52a7b8fe6ff35120dad5ae9b87e8
Shane Hudson <shane@shanehudson.net>
2016-04-17 19:09:34 +0100

    Adding Person entity

---
+++ single-person.php
@@ -0,0 +1,16 @@
+<?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<section>
+		<article>
+		    <?php
+		        $name = get_the_field('first_name') . " " . get_the_field('last_name');
+		        if ($preferred = get_the_field('preferred_name'))  {
+                    $name = $preferred;
+		        }
+			<h1><?php echo $name ?></h1>
+		</article>
+	</section>
+<?php endwhile; ?>
+
+<?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
