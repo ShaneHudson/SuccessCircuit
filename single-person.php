23948eb82ca18419ddb92f566f4cc2de2cf3c0b2
Shane Hudson <shane@shanehudson.net>
2016-04-17 19:36:10 +0100

    fixes

--- single-person.php
+++ single-person.php
@@ -35,8 +35,8 @@
             <?php
                 $interviews = get_field('related_interview');
 
-                if ($people): ?>
-                    <?php foreach( $interviews as $interview ): ?>
+                if ($interviews):
+                    foreach( $interviews as $interview ): ?>
                         <a href="<?php echo get_permalink( $interview->ID ); ?>"><?php echo get_the_title( $interview->ID ); ?></a>
                     <?php endforeach; ?>
                 <?php endif; ?>
